5268fa691ae58c775137915b7258b6989f22ef1b
espaus <espen.austefjord@hotmail.com>
2017-11-15 19:03:51 +0100

    added async tasks

--- Program.cs
+++ Program.cs
@@ -9,19 +9,22 @@ namespace TeleprompterConsole
     {
         static void Main(string[] args)
         {
+            ShowTeleprompter().Wait();
+            /*
             var lines = ReadFrom("sampleQuotes.txt");
             foreach (var line in lines)
             {
                 Console.Write(line);
                 if (!string.IsNullOrWhiteSpace(line))
                 {
-                    var pause = Task.Delay(50);
+                    var pause = Task.Delay(200);
                     // Synchronously waiting on a task is an
                     // anti-pattern. This will get fixed in later
                     // steps.
                     pause.Wait();
                 }
             }
+            */
         }
 
         static IEnumerable<string> ReadFrom(string file)
@@ -46,6 +49,40 @@ namespace TeleprompterConsole
                     yield return Environment.NewLine;
                 }
             }
+        }
+
+        private static async Task ShowTeleprompter()
+        {
+            var words = ReadFrom("sampleQuotes.txt");
+            foreach (var line in words)
+            {
+                Console.Write(line);
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    await Task.Delay(200);
+                }
+            }
+        }
+
+        private static async Task GetInput()
+        {
+            var delay = 200;
+            Action work = () =>
+            {
+                do
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.KeyChar == '>')
+                    {
+                        delay -= 10;
+                    }
+                    else if (key.KeyChar == '<')
+                    {
+                        delay += 10;
+                    }
+                } while (true);
+            };
+            await Task.Run(work);
         }
     }
 }
